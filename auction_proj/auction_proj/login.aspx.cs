614875a51e7ddb6c5db93f6e5e57b760f37d5f04
senio102 <senio102@VDI-CPSC-NTX-10>
2017-04-03 15:30:51 -0700

    loginpg1

--- auction_proj/auction_proj/login.aspx.cs
+++ auction_proj/auction_proj/login.aspx.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
 namespace auction_proj
 {
     public partial class login : System.Web.UI.Page
@@ -15,16 +17,11 @@ namespace auction_proj
 
         }
 
-        protected void LoginButton_Click(object sender, EventArgs e)
-        {
-
-            //Login1.UserName
-        }
-
         protected void Button1_Click(object sender, EventArgs e)
         {
             // In a using statement, acquire the SqlConnection as a resource.
             //
+
             using (SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\senio102\Source\Repos\auction_project\auction_proj\auction_proj\App_Data\Database1.mdf; Integrated Security = True"))
             {
                 //
@@ -35,18 +32,43 @@ namespace auction_proj
                 try
                 {
                     con.Open();
-                    SqlCommand add = new SqlCommand(@"INSERT INTO Table1 (username, password) VALUES ('user22', 'pwd')", con);
-                    add.ExecuteNonQuery();
                     SqlCommand cmd = new SqlCommand(@"SELECT Count(*) FROM Table1 
                                         WHERE username=@uname and 
                                         password=@pass", con);
-                    cmd.Parameters.AddWithValue("@uname", Login1.UserNameLabelText);
-                    cmd.Parameters.AddWithValue("@pass", Login1.PasswordLabelText);
+                    cmd.Parameters.AddWithValue("@uname", TextBox1.Text);
+                    cmd.Parameters.AddWithValue("@pass", TextBox2.Text);
                     int result = (int)cmd.ExecuteScalar();
                     if (result > 0)
-                        Label3.Text=("Login Success");
+                        Label3.Text = ("Login Success");
                     else
                         Label3.Text = ("Incorrect login");
+                }
+                catch (Exception ex)
+                {
+                    Label3.Text = ("Unexpected error:" + ex.Message);
+                }
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            // In a using statement, acquire the SqlConnection as a resource.
+            //
+
+            using (SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\senio102\Source\Repos\auction_project\auction_proj\auction_proj\App_Data\Database1.mdf; Integrated Security = True"))
+            {
+                //
+                // The following code uses an SqlCommand based on the SqlConnection.
+                //
+                //SqlCommand command = new SqlCommand("SELECT * FROM Table1", con);
+                //command.ExecuteNonQuery();
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(@"INSERT INTO Table1 (username, password) VALUES (@uname, @pass)", con);
+                    cmd.Parameters.AddWithValue("@uname", TextBox1.Text);
+                    cmd.Parameters.AddWithValue("@pass", TextBox2.Text);
+                    cmd.ExecuteNonQuery();
                 }
                 catch (Exception ex)
                 {
--- auction_proj/auction_proj/login.aspx.designer.cs
+++ auction_proj/auction_proj/login.aspx.designer.cs
@@ -31,13 +31,13 @@ namespace auction_proj {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
-        /// Login1 control.
+        /// Label3 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Login Login1;
+        protected global::System.Web.UI.WebControls.Label Label3;
         
         /// <summary>
         /// TextBox1 control.
@@ -49,6 +49,15 @@ namespace auction_proj {
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
         
         /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
@@ -58,12 +67,12 @@ namespace auction_proj {
         protected global::System.Web.UI.WebControls.Button Button1;
         
         /// <summary>
-        /// Label3 control.
+        /// Button3 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
+        protected global::System.Web.UI.WebControls.Button Button3;
     }
 }
