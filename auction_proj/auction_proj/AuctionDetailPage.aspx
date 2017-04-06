ad66cf9db88529befbc3f335774916ab54da2367
trans101 <trans101@VDI-CPSC-NTX-03>
2017-04-05 19:35:19 -0700

    browse functionality

---
+++ auction_proj/auction_proj/AuctionDetailPage.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AuctionDetailPage.aspx.cs" Inherits="auction_proj.AuctionDetailPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <asp:Label ID="auctName" runat="server" Text="NULL Auction" Font-Bold="true"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="auctOrg" runat="server" Text="Organization: "></asp:Label>
+        <br />
+        <asp:Label ID="auctLocation" runat="server" Text="Location: "></asp:Label>
+        <br />
+        <asp:Label ID="auctDate" runat="server" Text="Date: "></asp:Label>
+        <br />
+        <asp:Label ID="auctTime" runat="server" Text="Time: "></asp:Label>
+        <br />
+        <asp:Label ID="auctNumItems" runat="server" Text="Items: "></asp:Label>
+        <br />
+        <asp:Label ID="auctNumBidders" runat="server" Text="Number Of Bidders: "></asp:Label>
+        <br />
+
+        <asp:Button ID="backButt" runat="server" Text="Back To Browse" OnClick="backButt_Click" />
+        
+
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ auction_proj/auction_proj/AuctionDetailPage.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace auction_proj
+{
+    
+    public partial class AuctionDetailPage : System.Web.UI.Page
+    {
+        private static String currentAuction;
+        private String date, time, org, numItems, location, registeredBidders;
+
+        protected void backButt_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/SelectAuction.aspx");
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            auctName.Text = currentAuction + " Details";
+
+            //get the auction information from database
+
+            org = "Chapman University";
+            location = "1 University Dr \n Orange, CA \n 92868";
+            time = "8:30 AM PST";
+            date = "19 April 2017 ";
+            numItems = "150 items";
+            registeredBidders = "3,500 persons";
+
+            auctOrg.Text += org ;
+            auctLocation.Text += location;
+            auctTime.Text += time;
+            auctDate.Text += date;
+            auctNumItems.Text +=numItems;
+            auctNumBidders.Text +=registeredBidders;
+
+        }
+        public static void setAuction(String auctionName)
+        {
+            currentAuction = auctionName;
+        }
+
+    }
+
+}
