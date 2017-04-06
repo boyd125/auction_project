93101c530d9ce7d9105a0a7368a5d1a31f5c2460
boyd125 <boyd125@VDI-CPSC-NTX-10>
2017-04-05 20:17:25 -0700

    Center Login

--- auction_proj/auction_proj/login.aspx.designer.cs
+++ auction_proj/auction_proj/login.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace auction_proj {
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
         
         /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
         /// TextBox2 control.
         /// </summary>
         /// <remarks>
