290af8ddc632e91c5dc5d3c9e1a208dea2dc4b61
Chad307 <johns428@mail.chapman.edu>
2017-04-19 11:43:46 -0700

    Add files via upload

--- AC_DatabaseQueries/AC Create Query.sql
+++ AC_DatabaseQueries/AC Create Query.sql
@@ -1,24 +1,45 @@
 --create tables
 
---auction tables
+--base tables
 
 create table Auction_Org(
 	org varchar(50),
 	primary key(org)
 )
 
+create table Employee_Account(
+	account_email varchar(50),
+	account_password varchar(50)
+	primary key(account_email)
+)
+
+create table NPO_Rep_Account(
+	account_email varchar(50),
+	account_password varchar(50),
+	primary key (account_email)
+)
+
+create table Item_Auction_ID(
+	item_name varchar(50),
+	auction varchar(50),
+	id int,
+	primary key(id)
+)
+
+create table Bidder_Account(
+	account_email varchar(50),
+	account_password varchar(50),
+	primary key(account_email)
+)
+
+--foreign tables
+
 create table Auction_Contact(
 	org varchar(50),
 	contact varchar(50),
 	foreign key(org) references Auction_Org(org),
-	primary key(contact)
-)
-
-create table Auction_Contact_Phone(
-	contact varchar(50),
-	phone varchar(12),
-	foreign key(contact) references Auction_Contact(contact) on update cascade,
-	primary key(contact, phone)	
+	foreign key(contact) references NPO_Rep_Account(account_email),
+	primary key(org, contact)
 )
 
 create table Auction_Date_Time(
@@ -32,7 +53,8 @@ create table Auction_Intake(
 	org varchar(50),
 	intake varchar(50),
 	foreign key(org) references Auction_Org(org),
-	primary key(org)
+	foreign key(intake) references Employee_Account(account_email),
+	primary key(org, intake)
 )
 
 create table Auction_Exp_Num_Items(
@@ -49,13 +71,25 @@ create table Auction_Comments(
 	primary key(org)
 )
 
---item tables
+create table Employee_Name(
+	employee varchar(50),
+	full_name varchar(50),
+	foreign key (employee) references Employee_Account (account_email),
+	primary key (employee)
+)
 
-create table Item_Auction_ID(
-	item_name varchar(50),
-	auction varchar(50),
-	id int,
-	primary key(id)
+create table NPO_Rep_Name(
+	rep varchar(50),
+	full_name varchar(50),
+	foreign key(rep) references NPO_Rep_Account(account_email),
+	primary key(rep)
+)
+
+create table NPO_Rep_Phone(
+	rep varchar(50),
+	phone varchar(14),
+	foreign key(rep) references NPO_Rep_Account(account_email),	
+	primary key(rep)
 )
 
 create table Item_Quantity(
@@ -107,14 +141,6 @@ create table Item_Photo(
 	primary key (id)
 )
 
---bidder tables
-
-create table Bidder_Account(
-	account_email varchar(50),
-	account_password varchar(50),
-	primary key(account_email)
-)
-
 create table Bidder_Name(
 	bidder varchar(50),
 	full_name varchar(50),
@@ -124,7 +150,7 @@ create table Bidder_Name(
 
 create table Bidder_Phone(
 	bidder varchar(50),
-	phone varchar(12),
+	phone varchar(14),
 	foreign key(bidder) references Bidder_Account(account_email),
 	primary key(bidder)
 )
@@ -143,8 +169,6 @@ create table Bidder_Street_Address(
 	primary key(street)
 )
 
---address table
-
 create table Full_Address(
 	street varchar(50),
 	city varchar(50),
@@ -153,8 +177,6 @@ create table Full_Address(
 	foreign key(street) references Bidder_Street_Address(street) on update cascade,
 	primary key(street)
 )
-
---bid table
 
 create table Bid(
 	bidder varchar(50),
@@ -167,36 +189,5 @@ create table Bid(
 	primary key(bidder, auction, item)
 )
 
---employee tables
 
-create table Employee_Account(
-	account_email varchar(50),
-	account_password varchar(50)
-	primary key(account_email)
-)
-
-create table Employee_Name(
-	employee varchar(50),
-	full_name varchar(50),
-	foreign key (employee) references Employee_Account (account_email),
-	primary key (employee)
-)
-
---npo tables
-
-create table NPO_Rep_Account(
-	account_email varchar(50),
-	account_password varchar(50),
-	primary key (account_email)
-)
-
-create table NPO_Rep_Org(
-	rep varchar(50),
-	org varchar(50),
-	foreign key (rep) references NPO_Rep_Account (account_email),
-	foreign key (org) references Auction_Org (org) on update cascade,
-	primary key (rep, org)
-)
-
-
-
+--select org from Auction_Contact where contact = 'email'
--- AC_DatabaseQueries/NPO_Rep_All View.sql
+++ AC_DatabaseQueries/NPO_Rep_All View.sql
@@ -1,5 +1,6 @@
 create view NPO_Rep_All
 	as
-		select account_email, account_password, org
-			from NPO_Rep_Account, NPO_Rep_Org
-				where account_email = rep
+		select NPO_Rep_Account.account_email, NPO_Rep_Account.account_password, full_name, phone
+			from NPO_Rep_Account, NPO_Rep_Name, NPO_Rep_Phone
+				where NPO_Rep_Account.account_email = NPO_Rep_Name.rep
+				and NPO_Rep_Account.account_email = NPO_Rep_Phone.rep
