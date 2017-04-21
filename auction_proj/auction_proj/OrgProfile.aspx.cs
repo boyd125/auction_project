04b3cb382b80dc1a04dc9ae49fc90a788d16b59a
trans101 <trans101@VDI-CPSC-07>
2017-04-20 18:11:16 -0700

    org profile

---
+++ auction_proj/auction_proj/OrgProfile.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace auction_proj
+{
+    public partial class OrgProfile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
